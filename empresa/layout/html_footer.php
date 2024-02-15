480c697d958605fc457455db208780a68064686a
Stelio Acácio Mondlane <109701079+Stelio-IN@users.noreply.github.com>
2024-02-15 16:32:06 +0200

    remove thinks

--- empresa/layout/html_footer.php
+++
@@ -1,6 +0,0 @@
-    <script src="https://kit.fontawesome.com/18e4eef65a.js" crossorigin="anonymous"></script>
-   
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-</body>
-</html>
